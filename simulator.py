91ace64351f5673491b9d3241a1a589e1741bece
Janderson <ffjanderson@gmail.com>
2024-10-19 20:42:44 -0300

    <improves> varios

--- simulator.py
+++ simulator.py
@@ -14,14 +14,14 @@ def corr2cov(p: np.ndarray, s: np.ndarray) -> np.ndarray:
     return d @ p @ d
 
 
-def plotter(dataframe, risk=0.1, n_assets=21, show_chart=True):
+def plotter(dataframe, risk=0.1, params_num_asset=21, show_chart=True):
     plt.figure(figsize=(15, 5))
     plt.plot(dataframe)
     plt.title("The Holy Grail of Ray Dalio")
     plt.legend(dataframe.columns)
     plt.xlabel("# of Assets/Alphas in Portfolio")
     plt.ylabel("Annual Portfolio Std.Deviation")
-    plt.xticks(list(range(0, n_assets-1)))
+    plt.xticks(list(range(0, params_num_asset-1)))
     #plt.yticks(np.arange(risk, 0, -0.1))
     print(f"showchart: {show_chart}")
     if show_chart:
@@ -38,13 +38,13 @@ def transform_input_correlations(correlations_str):
     return [int(item) for item in correlations_str.split(",")]
 
 
-def simulator(correlations, param_risk=.10):
-    param_means = param_stddev = param_risk
+def simulator(correlations, param_return=.10, params_num_asset=20):
+    param_means = param_stddev = param_return
     df = pd.DataFrame()
     results = []
     for correlation in build_fraction_list(correlations):
         result_list = []
-        for num_assets in range(1, 21):
+        for num_assets in range(1, params_num_asset+1):
             corr_matrix = np.zeros((num_assets, num_assets), float)
             corr_matrix.fill(correlation)
             np.fill_diagonal(corr_matrix, 1)
@@ -79,14 +79,16 @@ def simulator(correlations, param_risk=.10):
 
 @click.command("simulator")
 @click.option("--savefigure", default=False)
-@click.option("--correlations", default="60,40,20,10,0")
-@click.option("--risk", default=10)
-def cmd_simulator(savefigure, correlations, risk):
+@click.option("--correlations", default="60,40,20,10,5")
+@click.option("--expected_return", "expected_return", default=10, type=click.FLOAT)
+@click.option("--num_assets", "num_assets", default=20)
+def cmd_simulator(savefigure, correlations, expected_return, num_assets):
     print(f"Simulator of Ray Dalio 'Holy Grail' built by: {AUTHOR}")
     result_df = simulator(transform_input_correlations(correlations),
-                          param_risk=float(risk)/100)
+                          param_return=float(expected_return)/100,
+                          params_num_asset=num_assets)
     #print(result_df.tail())
-    plotter(result_df, show_chart=(not savefigure))
+    plotter(result_df, show_chart=(not savefigure), params_num_asset=num_assets)
 
 
 if __name__ == "__main__":
